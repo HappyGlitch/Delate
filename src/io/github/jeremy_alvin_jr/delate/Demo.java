38f9d9435a78a90492107a3b6db67dcc88de2b6b
Barlomiej Dygos <gltchr@outlook.com>
2023-02-23 22:44:20 +0100

    playback test

--- src/io/github/jeremy_alvin_jr/delate/Demo.java
+++ src/io/github/jeremy_alvin_jr/delate/Demo.java
@@ -1,2 +1,33 @@
-package io.github.jeremy_alvin_jr.delate;public class Demo {
+package io.github.jeremy_alvin_jr.delate;
+
+import io.github.jeremy_alvin_jr.delate.playback.AudioStreamer;
+
+import java.util.Arrays;
+
+public class Demo {
+    public static void main(String[] args) {
+        AudioStreamer.Device devices[] = AudioStreamer.getAvailableDevices();
+        for(AudioStreamer.Device d: devices) {
+            System.out.println(d);
+            if(!d.isOutputSupported()) {
+                System.out.println("pass!\n\n");
+                try {
+                    Thread.sleep(2000);
+                } catch(Exception e) {
+                    e.printStackTrace();
+                }
+                continue;
+            }
+            try {
+                new AudioStreamer(d).play();
+            } catch(Exception e) {
+                e.printStackTrace();
+                try {
+                    Thread.sleep(2000);
+                } catch (Exception g) {
+
+                }
+            }
+        }
+    }
 }
